5578264c4dff359e02616909b4e857c58b824c84
cindychangy <cindyisachang@gmail.com>
2022-07-13 09:07:55 +0200

    adding guide data

---
+++ src/components/guide/guide-data.tsx
@@ -0,0 +1,62 @@
+export const GUIDE_SEARCH = [
+  {
+    title: 'Add your data',
+    description:
+      'Adding data is fast and easy with our out-of-the-box integrations. Quickly monitor popular cloud services, applications, systems, containers, and more.',
+    completed: false,
+  },
+  {
+    title: 'Tour Elastic Search',
+    description:
+      'See how you can easily unlock the power of the Elastic search platform to query your logs, view your infrastructure, monitor applications, visualize your data, and more.',
+    completed: true,
+  },
+];
+
+export const GUIDE_ObSERVABILITY = [
+  {
+    title: 'Monitor your environment',
+    description:
+      'Adding data is fast and easy with our out-of-the-box integrations. Quickly monitor popular cloud services, applications, systems, containers, and more.',
+    completed: false,
+  },
+  {
+    title: 'Tour Elastic Observability',
+    description:
+      'See how you can easily unlock the power of the Elastic search platform to query your logs, view your infrastructure, monitor applications, visualize your data, and more.',
+    completed: true,
+  },
+  {
+    title: 'Explore your data',
+    description:
+      'Query and analyze your data using the Discover app. Find any detail in your data, no matter how obscure or complex.',
+    completed: false,
+  },
+  {
+    title: 'Do more with Elastic Observability',
+    description:
+      'See how you can enhance visibility into your environment with capabilities from Elastic like Application Performance Monitoring (APM), Uptime Monitoring, and more.',
+    completed: false,
+  },
+];
+
+export const GUIDE_SECURITY = [
+  {
+    title: 'Monitor your environment',
+    description:
+      'Adding data is fast and easy with our out-of-the-box integrations. Quickly monitor popular cloud services, applications, systems, containers, and more.',
+    completed: false,
+  },
+  {
+    title: 'Explore your data',
+    description:
+      'Query and analyze your data using the Discover app. Find any detail in your data, no matter how obscure or complex.',
+    completed: false,
+  },
+  {
+    title: 'Do more with Elastic Security',
+    description:
+      'See how you can enhance visibility into your environment with capabilities from Elastic like Application Performance Monitoring (APM), Uptime Monitoring, and more.',
+    completed: false,
+  },
+];
