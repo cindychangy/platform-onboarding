c360eaedf94b87d6fdd17207e0e84a17b5207589
cindychangy <cindyisachang@gmail.com>
2022-07-13 13:11:17 +0200

    step page cleanup

--- src/pages/8.6/guided-setup/step.tsx
+++ src/pages/8.6/guided-setup/step.tsx
@@ -1,45 +1,7 @@
 import { useState } from 'react';
-import {
-  EuiBadge,
-  EuiButtonEmpty,
-  EuiSpacer,
-  EuiTitle,
-  EuiText,
-  EuiLink,
-  EuiTextColor,
-  EuiCard,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiIcon,
-  EuiHorizontalRule,
-  useEuiTheme,
-} from '@elastic/eui';
+import { EuiTitle, useEuiTheme } from '@elastic/eui';
 import KibanaLayout from '../../../layouts/kibana';
 import { gettingSetupStyles } from '../../../styles/guided-setup.styles';
-
-const CARDS = [
-  {
-    title: 'Search my data',
-    description:
-      'Create a search experience for your websites, applications, workplace content, or anything in between.',
-    icon: 'logoElasticsearch',
-    footer: <EuiBadge color="hollow">Search</EuiBadge>,
-  },
-  {
-    title: 'Observe my infrastructure',
-    description:
-      'Monitor your infrastructure by consolidating your logs, metrics, and traces for end‑to‑end observability.',
-    icon: 'logoObservability',
-    footer: <EuiBadge color="hollow">Observability</EuiBadge>,
-  },
-  {
-    title: 'Protect my environment',
-    description:
-      'Protect your environment by unifying SIEM, endpoint security, and cloud security to protect against threats.',
-    icon: 'logoSecurity',
-    footer: <EuiBadge color="hollow">Security</EuiBadge>,
-  },
-];
 
 const GuideSetup = () => {
   const { euiTheme } = useEuiTheme();
